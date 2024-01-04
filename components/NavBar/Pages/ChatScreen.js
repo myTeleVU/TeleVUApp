fc6d47c0bedfffd03211dad2fb338551c5f78bbd
adamchamp <adamchamp069@gmail.com>
2024-01-03 21:29:21 -0500

    Importing issues fixed

--- components/NavBar/Pages/ChatScreen.js
+++ components/NavBar/Pages/ChatScreen.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
+import { ChatBar } from '../../Session/ChatBar/ChatBar.component'
 
 export default function ChatScreen({ navigation }) {
     return (
@@ -7,6 +8,7 @@ export default function ChatScreen({ navigation }) {
             <Text
                 onPress={() => navigation.navigate('Home')}
                 style={{ fontSize: 26, fontWeight: 'bold' }}>Chat Screen, route to chatvu</Text>
+                <ChatBar/>
         </View>
     );
 }
