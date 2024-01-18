131c71edba80e736b9aa691fbba51f2e0550f090
Eden Nalian <122690785+EdenNal@users.noreply.github.com>
2024-01-18 14:49:29 -0500

    test

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-//import NavBar from './components/NavBar/NavBar'
+import NavBar from './components/NavBar/NavBar'
 
 export default function App() {
   return (
-    
+    <View>
       <NavBar/>
+      <Text >
+        test
+        </Text>
+        </View>
     
   );
 }
